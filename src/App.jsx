552f1694940bd73840593fd2a53626dd297096ec
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-13 06:58:34 +0400

    improve app.tsx code display

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import AddTodo from './components/AddTodo'
-import TodoList from './components/TodoList'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
-    const [todos,setTodos]=useState([])
-    const [newValue,setNewValue]=useState("")
-    const [filter, setFilter] = useState("all");
-    const [isDarkMode, setIsDarkMode] = useState(true);
+  const [todos, setTodos] = useState([]);
+  const [newValue, setNewValue] = useState("");
+  const [filter, setFilter] = useState("all");
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-    const filtredTodos=todos.filter(todo =>
-        filter === "All"? true:
-        filter === "Active"? !todo.status :
-        filter ==="Completed"? todo.status :
-        true
-        );
+  const filtredTodos = todos.filter((todo) =>
+    filter === "All"
+      ? true
+      : filter === "Active"
+      ? !todo.status
+      : filter === "Completed"
+      ? todo.status
+      : true
+  );
   return (
     <>
       <main
@@ -25,12 +28,12 @@ function App() {
             : "bg-[#FAFAFA]text-black bg-mountains"
         }`}
       >
-        <div className="w-[540px] mx-auto pt-[50px]">
-          <div className="flex justify-between items-center">
-            <h1 className="text-white text-[40px] font-bold">TODO</h1>
+        <div className='w-[540px] mx-auto pt-[50px]'>
+          <div className='flex justify-between items-center'>
+            <h1 className='text-white text-[40px] font-bold'>TODO</h1>
             <img
               onClick={() => setIsDarkMode(!isDarkMode)}
-              src="/images/icon-sun.svg"
+              src='/images/icon-sun.svg'
             />
           </div>
           <AddTodo
@@ -50,4 +53,4 @@ function App() {
     </>
   );
 }
-export default App
+export default App;
