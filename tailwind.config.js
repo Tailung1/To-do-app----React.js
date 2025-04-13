78cab23f10c4db6438659ba88b54136a2e1dea0e
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-13 06:59:43 +0400

    update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend:{
-        colors: {
-            'dark':'#171823',
-            "dark-blue":"#25273D",
-            "indicatorColor": "#5B5E7E"
-        },
-        backgroundImage: {
-            "hero-image": "url('/images/bg-desktop-dark.jpg')",
-            "mountains": "url('/images/bg-desktop-light.jpg')"
-        },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#171823",
+        "dark-blue": "#25273D",
+        indicatorColor: "#5B5E7E",
+      },
+      backgroundImage: {
+        "hero-image": "url('/images/bg-desktop-dark.jpg')",
+        mountains: "url('/images/bg-desktop-light.jpg')",
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
